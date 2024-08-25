674bf282ad469b72b7302f7fe6a7a0b53c610d62
Tyler Liu <tyler4long@gmail.com>
2024-08-25 10:35:47 -0700

    Upgrade dependencies

--- src/app.tsx
+++ src/app.tsx
@@ -1,34 +1,29 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { auto } from 'manate/react';
+import { ContactShadows, OrbitControls } from '@react-three/drei';
+import { useControls } from 'leva';
 
-import type { Store } from './store';
-
-const App = (props: { store: Store }) => {
-  const render = () => {
-    return (
-      <Canvas camera={{ position: [0, 3, 8] }}>
-        {/* Lighting */}
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[0, 3, 5]} intensity={0.5} />
-
-        {/* Objects */}
-        <mesh position={[-3, 0, 0]}>
-          <boxGeometry />
-          <meshStandardMaterial color="red" />
-        </mesh>
-        <mesh position={[0, 0, 0]}>
-          <boxGeometry />
-          <meshStandardMaterial color="blue" />
-        </mesh>
-        <mesh position={[3, 0, 0]}>
-          <boxGeometry />
-          <meshStandardMaterial color="green" />
-        </mesh>
-      </Canvas>
-    );
-  };
-  return auto(render, props);
-};
+const App = auto(() => {
+  const { cubeInAir } = useControls({
+    cubeInAir: false,
+  });
+  return (
+    <Canvas camera={{ position: [0, 3, 3] }}>
+      <OrbitControls />
+      <ContactShadows />
+      <directionalLight position={[5, 5, 5]} intensity={0.2} />
+      <directionalLight position={[-5, 5, 5]} intensity={0.8} />
+      <mesh rotation-y={Math.PI / 4} position-y={cubeInAir ? 1 : 0}>
+        <boxGeometry />
+        <meshStandardMaterial color="green" />
+      </mesh>
+      <mesh position={[1, 1, 1]}>
+        <sphereGeometry args={[0.5, 32, 32]} />
+        <meshStandardMaterial color="white" />
+      </mesh>
+    </Canvas>
+  );
+});
 
 export default App;
